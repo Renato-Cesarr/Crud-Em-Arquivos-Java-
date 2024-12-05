1be9fad859b02d1ce874d5618978ee7e2b7b28e5
Renato-Cesarr <renatocesaroficial01@gmail.com>
2024-12-05 11:39:09 -0300

    quase finalizado, esperando feedback samuca

--- src/CrudBasico/CrudInsert.java
+++ src/CrudBasico/CrudInsert.java
@@ -11,68 +11,70 @@ import java.util.Scanner;
 
 import CrudValidações.Validacoes;
 import Main.MenuDaTelaMain;
+import Main.Personagem;
 import Main.desenhoDeTelas;
 
 public class CrudInsert {
 
 	static Scanner scan = new Scanner(System.in);
-
 	static int id = 1;
-	public static String nomeDoPersonagem;
-	public static String nomeDaClasse;
-	public static String tipoDeArma;
-	public static int pontosDeHp;
-	public static int pontosDeMana;
-	
+
 	static String caminhoParaDiretorio = "/home/almaviva-linux/eclipse-workspace/CrudEmArquivos/ArquivoDoCrud";
 	static String nomeArquivo = "CrudBasico.txt";
 	static File diretorio = new File(caminhoParaDiretorio);
-	static HashMap<Integer, String> idDoUsuario = new HashMap<Integer, String>();
+	static HashMap<Integer, Personagem> idDoUsuario = new HashMap<Integer, Personagem>();
 
 	public static void inserir() throws IOException, InterruptedException {
 		desenhoDeTelas.exibirTelaInsert();
 		ordemIDs();
-		
-		inserirDadosUsuario();
-		
 
-		idDoUsuario.put(id, nomeDoPersonagem);
+		Personagem personagem = inserirDadosUsuario();
 
-		String textoDoArquivo = id + "," + nomeDoPersonagem + "," + nomeDaClasse +"," + tipoDeArma + "," + pontosDeHp + "," + pontosDeMana + "\n";
+		idDoUsuario.put(id, personagem);
+
+		String textoDoArquivo = id + "," + personagem.getNomeDoPersonagem() + "," + personagem.getNomeDaClasse() + ","
+				+ personagem.getTipoDeArma() + "," + personagem.getPontosDeHp() + "," + personagem.getPontosDeMana()
+				+ "\n";
 
 		verificarSeDiretorioExisteSalvar(textoDoArquivo);
 
 		id++;
-		
-        Thread.sleep(3000);
+
+		Thread.sleep(3000);
 		MenuDaTelaMain.chamarMenu();
-
 	}
-	public static void inserirDadosUsuario() {
 
+	public static Personagem inserirDadosUsuario() {
 		Validacoes v = new Validacoes();
 		boolean ValidacaoDeInformacoes = false;
+		String nomeDoPersonagem = "";
+		String nomeDaClasse = "";
+		String tipoDeArma = "";
+		int pontosDeHp = 0;
+		int pontosDeMana = 0;
+
 		do {
 			try {
-				System.out.println("Insera os dados:");
+				System.out.println("Insira os dados:");
 				System.out.println("Digite o nome do personagem: ");
 				nomeDoPersonagem = scan.nextLine().toLowerCase();
-				System.out.println("Digite qual sua classe ");
+				System.out.println("Digite qual sua classe: ");
 				nomeDaClasse = scan.nextLine().toLowerCase();
 				System.out.println("Escolha sua arma: ");
 				tipoDeArma = scan.nextLine().toLowerCase();
-				System.out.println("Distribua 200 pontos entre " + "\033[31mVIDA\033[0m" + " e " + "\033[34mMana\033[0m");
+				System.out
+						.println("Distribua 200 pontos entre " + "\033[31mVIDA\033[0m" + " e " + "\033[34mMana\033[0m");
 				pontosDeHp = scan.nextInt();
 				pontosDeMana = scan.nextInt();
 				scan.nextLine();
 			} catch (Exception e) {
-				System.out.println("nao digite texto no lugar dos numeros!");
-				//ValidacaoDeInformacoes = true;
+				System.out.println("Não digite texto no lugar dos números!");
 				scan.nextLine();
 			}
-			ValidacaoDeInformacoes = v.validarEntradaUpdate(nomeDoPersonagem,nomeDaClasse,tipoDeArma,pontosDeHp,pontosDeMana);
-
+			ValidacaoDeInformacoes = Validacoes.validarEntradaUpdate(nomeDoPersonagem, nomeDaClasse, tipoDeArma, pontosDeHp,pontosDeMana);
 		} while (!ValidacaoDeInformacoes);
+
+		return new Personagem(nomeDoPersonagem, nomeDaClasse, tipoDeArma, pontosDeHp, pontosDeMana);
 	}
 
 	public static void verificarSeDiretorioExisteSalvar(String textoDoArquivo) {
@@ -91,7 +93,6 @@ public class CrudInsert {
 	}
 
 	public static void ordemIDs() throws FileNotFoundException, IOException {
-
 		File arquivo = new File(diretorio, nomeArquivo);
 		if (arquivo.exists()) {
 			try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
@@ -105,7 +106,5 @@ public class CrudInsert {
 				}
 			}
 		}
-
 	}
-
 }
---
+++ src/Main/Personagem.java
@@ -0,0 +1,38 @@
+package Main;
+
+public class Personagem {
+	private String nomeDoPersonagem;
+	private String nomeDaClasse;
+	private String tipoDeArma;
+	private int pontosDeHp;
+	private int pontosDeMana;
+
+	public Personagem(String nomeDoPersonagem, String nomeDaClasse, String tipoDeArma, int pontosDeHp,
+		int pontosDeMana) {
+		this.nomeDoPersonagem = nomeDoPersonagem;
+		this.nomeDaClasse = nomeDaClasse;
+		this.tipoDeArma = tipoDeArma;
+		this.pontosDeHp = pontosDeHp;
+		this.pontosDeMana = pontosDeMana;
+	}
+
+	public String getNomeDoPersonagem() {
+		return nomeDoPersonagem;
+	}
+
+	public String getNomeDaClasse() {
+		return nomeDaClasse;
+	}
+
+	public String getTipoDeArma() {
+		return tipoDeArma;
+	}
+
+	public int getPontosDeHp() {
+		return pontosDeHp;
+	}
+
+	public int getPontosDeMana() {
+		return pontosDeMana;
+	}
+}
